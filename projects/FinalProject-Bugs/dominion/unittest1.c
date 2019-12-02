e1b0ea0350b6debf0f3f0fcb539806fc99d19854
Dan Negovan <Dan@Dans-Hack-Pro.local>
2019-12-01 21:55:34 -0800

    dan's unit test changes

--- projects/FinalProject-Bugs/dominion/unittest1.c
+++ projects/FinalProject-Bugs/dominion/unittest1.c
@@ -1,5 +1,6 @@
 #include "dominion.h"
 #include <stdio.h>
+#include "dansFunctions.h"
 
 
 
@@ -7,5 +8,34 @@
 
 int main ()  {
 
+    int test;               //holds results of function calls (-1, 0...)
+
+    // set card array
+    int k[10] = { ambassador, council_room, tribute, gardens, mine
+    , remodel, smithy, village, baron, great_hall };
+
+    // declare the game state
+    struct gameState G;
+
+    //--------------------------------------------------------------------------
+    //TEST
+    //--------------------------------------------------------------------------
+    // set the state of variables
+    memset(&G, 0, sizeof(struct gameState));
+    int r = initializeGame(2, k, 15351, &G);
+
+    printf("\n\nUNIT TEST BUG 1 -- Test commencing!\n\n");
+    G.handCount[0] = 2;
+    G.hand[0][0] = mine;
+    G.hand[0][1] = copper;
+
+    G.discardCount[0] = 0;  //empty discard pile for currentPlayer
+           
+    test = cardEffect(mine, 1, silver, -1, &G, 0, -1);  //call function
+
+    //currentplayer discard pile should still be empty
+    danAssert(G.discardCount[0] == 1, "BUG1: G.discardCount[0] == 0");
+
+    printf("\nUNIT TEST BUG 1 -- Test completed!\n\n");
     return 0;
 }
--- projects/FinalProject-Bugs/dominion/unittest10.c
+++ projects/FinalProject-Bugs/dominion/unittest10.c
@@ -1,11 +1,45 @@
 #include "dominion.h"
 #include <stdio.h>
-
-
+#include "dansFunctions.h"
 
 
 
 int main ()  {
 
+    int test;               //holds results of function calls (-1, 0...)
+
+    // set card array
+    int k[10] = { ambassador, council_room, feast, gardens, mine
+    , remodel, smithy, village, baron, great_hall };
+
+    // declare the game state
+    struct gameState G;
+
+    //--------------------------------------------------------------------------
+    //TEST
+    //--------------------------------------------------------------------------
+    // set the state of variables
+    memset(&G, 0, sizeof(struct gameState));
+    int r = initializeGame(2, k, 15351, &G);  //2 player game
+
+    printf("\n\nUNIT TEST BUG 10 -- Test commencing!\n\n");
+    G.handCount[0] = 3;
+    G.hand[0][0] = mine;
+    G.hand[0][1] = mine;            //card to reveal in pos 1
+    G.hand[0][2] = mine;      
+    G.hand[0][3] = ambassador;      //ambassador in position 3
+
+    G.whoseTurn = 0;
+
+    G.handCount[1] = 0;  //other player has no cards               
+
+    test = cardEffect(ambassador, 1, 2, -1, &G, 3, -1);  //call function
+
+    //current player 0 should be down to 1 cards
+    danAssert(G.handCount[0] == 1, "3C: G.handCount[0] == 1");
+    //other players should have 1 more card(mine) than before
+    danAssert(G.handCount[1] == 1, "3C: G.handCount[1] == 1");
+
+    printf("\nUNIT TEST BUG 10 -- Test completed!\n\n\n");
     return 0;
 }
